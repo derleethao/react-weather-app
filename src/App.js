ea4bb76b7c02b7024abf3b8a4ef2da655fdde266
derleethao <der.leethao@gmail.com>
2022-09-09 13:42:29 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Der Lee-Thao and is{" "}
-        <a href="https://github.com/derleethao/react-weather-app">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Der Lee-Thao and is{" "}
+          <a href="https://github.com/derleethao/react-weather-app">
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

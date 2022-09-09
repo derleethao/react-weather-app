1491346378de1fb1ef069f15f59f18ac06101d0a
derleethao <der.leethao@gmail.com>
2022-09-09 13:34:14 -0500

    Created the Github repo URL

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by Der Lee-Thao and is{" "}
-        <a
-          href="https://github.com/derleethao/react-weather-app"
-          target="_blank"
-        >
+        <a href="https://github.com/derleethao/react-weather-app">
           open-sourced on Github
         </a>
       </footer>

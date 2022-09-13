bc3575918a7fd5195f24e813b8669421ae264768
derleethao <der.leethao@gmail.com>
2022-09-13 14:31:56 -0500

    Added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           This project was coded by Der Lee-Thao and is{" "}
           <a href="https://github.com/derleethao/react-weather-app">

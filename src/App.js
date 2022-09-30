acfd02f1aa83b2145f28593675ac51a8b035daef
derleethao <der.leethao@gmail.com>
2022-09-30 15:11:35 -0500

    Added the 5 day forecast

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ export default function App() {
         <Weather defaultCity="New York" />
         <footer>
           This project was coded by Der Lee-Thao and is{" "}
-          <a href="https://github.com/derleethao/react-weather-app">
+          <a
+            href="https://github.com/derleethao/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             open-sourced on Github
           </a>
         </footer>
